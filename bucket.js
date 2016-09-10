d4114e14947a1a5e7fb431f5ff01ce27fe07f1eb
Oliver Stigley <oliverstigley@gmail.com>
2016-09-10 22:51:51 +1200

    kata branch

--- bucket.js
+++ bucket.js
@@ -1,45 +1,22 @@
-const filler = (state) => ({
-	fill: (amount) => {
-		state.level+= amount
-	}
+const filler = state => ({
+	
 })
 
-const tipper = (state) => ({
-	tip: () => {
-		if (state.level > state.volume) {
-			state.level = 0
-			state.children.map((child, i) => {
-				child.fill(state.distribution[i]*state.volume)
-				child.tip()
-			})
-		}
-	}
+const tipper = state => ({
+	
 })
 
 const getState = state => ({
-	getState: () => state
+	
 })
 
-const reader = (state) => ({
-	read: () => ' '.repeat(state.level).bgCyan + ' '.repeat(Math.floor(state.volume-state.level)).bgBlue
+const reader = state => ({
+
 })
 
 
-const Bucket = (volume = 10, children=[], distribution=[]  ) => {
-	let state = {
-		volume,
-		children,
-		distribution,
-		level: 0,
-	}
+const Bucket = ( ) => {
 
-	return Object.assign(
-		{},
-		filler(state),
-		tipper(state),
-		reader(state),
-		getState(state)
-	)
 }
 
 export {filler, tipper, getState, Bucket}
--- fill-em-up.js
+++ fill-em-up.js
@@ -1,31 +1,3 @@
 import clear from 'clear'
 import colors from 'colors'
 import {filler, tipper, Bucket} from './bucket.js'
-
-const bucket13 = Bucket(25)
-const bucket12 = Bucket(14, [bucket13], [0.5])
-const bucket11 = Bucket(10, [bucket13], [1])
-const bucket10 = Bucket(9, [bucket13], [0.8])
-
-const bucket9 = Bucket(11, [bucket11], [0.6])
-const bucket8 = Bucket(10, [bucket13, bucket10], [0.3, 0.7])
-
-const bucket7 = Bucket(13, [bucket10, bucket8, bucket9], [0.25, 0.25, 0.3])
-const bucket6 = Bucket(7, [bucket11, bucket9], [0.5, 0.5])
-const bucket5 = Bucket(8, [bucket8, bucket10, bucket13], [0.5, 0.25, 0.25])
-
-const bucket4 = Bucket(9, [bucket7, bucket12], [0.6, 0.4])
-const bucket3 = Bucket(11, [bucket6, bucket7], [0.3, 0.7]) 
-const bucket2 = Bucket(10, [bucket6, bucket8], [0.6,0.4])
-const bucket1 = Bucket(12, [bucket5, bucket10], [0.5,0.5])
-
-setInterval(()=> {
-	[bucket1, bucket2, bucket3, bucket4].map(bucket => {bucket.fill(0.2); bucket.tip()})
-	clear()
-	console.log('         ',bucket1.read(), '         ',bucket2.read(), '         ',bucket3.read(), '         ',bucket4.read())
-	console.log('                    ', bucket5.read(), '           ', bucket6.read(), '           ', bucket7.read())
-	console.log('                     ', bucket8.read(), '                ',bucket9.read())
-	console.log('                 ', bucket10.read(), '             ', bucket11.read(), '                 ',bucket12.read())
-	console.log('                              ',bucket13.read())
-}, 200)
-
